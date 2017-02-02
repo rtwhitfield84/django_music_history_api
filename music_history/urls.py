074f6425fc80955d5e191d6d3c8a2e42f051dcc3
Rico <richardthomaswhitfield@gmail.com>
2017-02-01 20:32:25 -0600

    updated admin and urls

--- music_history/urls.py
+++ music_history/urls.py
@@ -27,5 +27,6 @@ router.register(r'songs', views.SongViewSet)
 
 urlpatterns = [
 	url(r'^', include(router.urls)),
-	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+	url(r'^admin/', admin.site.urls),
+	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- music_history_api/admin.py
+++ music_history_api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from music_history_api.models import Artist, Genre, Album, Song
 
-# Register your models here.
+
+admin.site.register(Artist)
+admin.site.register(Genre)
+admin.site.register(Album)
+admin.site.register(Song)
